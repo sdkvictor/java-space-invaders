f5230fd6d3df1000944eefbf0d4fb84d634b1672
Carlos Estrada <carlos.deg02@hotmail.com>
2019-03-08 13:11:36 -0600

    Finished refactoring files

--- SpaceInvaders/src/spaceinvaders/Assets.java
+++ SpaceInvaders/src/spaceinvaders/Assets.java
@@ -12,28 +12,10 @@ import java.awt.image.BufferedImage;
  * @author charles
  */
 public class Assets {
-
-    public static BufferedImage background;
-    public static BufferedImage player;
-    public static BufferedImage enemy;
-    public static BufferedImage life;
-    public static BufferedImage gameover;
-    public static BufferedImage exploding;
     
-    public static SoundClip crashSound;
-    public static SoundClip eliminateSound;
-    public static SoundClip coinSound;
+    public static BufferedImage background;
     
     public static void init() {
         background = ImageLoader.loadImage("/images/space_background.jpg");
-        player = ImageLoader.loadImage("/images/player_super.png");
-        life = ImageLoader.loadImage("/images/heart.png");
-        enemy = ImageLoader.loadImage("/images/asteroid_enemy.png");
-        exploding = ImageLoader.loadImage("/images/explosion.png");
-        gameover = ImageLoader.loadImage("/images/game_over.png");
-        
-        crashSound = new SoundClip("/sounds/crash.wav");
-        eliminateSound = new SoundClip("/sounds/crash.wav");
-        coinSound = new SoundClip("/sounds/coin.wav");
     }
 }
