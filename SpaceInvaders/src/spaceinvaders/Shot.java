8c7ee4cc2d7d42155b7321b041d09f542481b596
Carlos Estrada <carlos.deg02@hotmail.com>
2019-03-08 17:23:09 -0600

    Added Bomb and Shot class, implemented Alien array in Game

---
+++ SpaceInvaders/src/spaceinvaders/Shot.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author charles
+ */
+public class Shot extends Item {
+
+    public Shot(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void render(Graphics g) {
+    }
+    
+}
